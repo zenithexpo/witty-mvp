0bfc6148baa6d109370b1f5a7d0cc7d692751d12
agaurav07 <abhishekgaurav0019@gmail.com>
2022-11-13 23:14:22 +0530

    Added navbar and searchbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Searchbar from './components/Searchbar/Searchbar';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Searchbar></Searchbar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Searchbar.css";
+import searchbarBg from "../../assets/backgroundSearch.png";
+
+function Searchbar() {
+  return (
+    <>
+      <div className="container mt-5 searchBar">
+        <div className="input-group mb-3 searchBarInputGroup">
+          <input
+            type="text"
+            className="form-control searchBarInput"
+            placeholder="Place name..."
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <span className="input-group-text searchBarIcon" id="basic-addon2">
+            Search
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Searchbar;
