b2198c5a26fd69a77dc94c6c8e38fcdef9747aa7
zenithexpo <112dikshaverma@gmail.com>
2022-12-13 22:41:34 +0530

    Updates

---
+++ precache-manifest.daa406edea5d0b25340c0c04142a98bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "087da54bba076a94629c",
+    "url": "/witty-mvp/static/css/main.12d8f241.chunk.css"
+  },
+  {
+    "revision": "087da54bba076a94629c",
+    "url": "/witty-mvp/static/js/main.087da54b.chunk.js"
+  },
+  {
+    "revision": "8453a32ff8b6ffbb93e8",
+    "url": "/witty-mvp/static/js/1.8453a32f.chunk.js"
+  },
+  {
+    "revision": "9a0cfbd4d2f9434e84c5",
+    "url": "/witty-mvp/static/js/2.9a0cfbd4.chunk.js"
+  },
+  {
+    "revision": "386fb9afc63a087ae8ce",
+    "url": "/witty-mvp/static/js/runtime~main.386fb9af.js"
+  },
+  {
+    "revision": "c95626437406e964b992839d41e9f8f5",
+    "url": "/witty-mvp/static/media/wittylogo.c9562643.png"
+  },
+  {
+    "revision": "b28c944c3de88be00801f27b7e1fc273",
+    "url": "/witty-mvp/static/media/backgroundSearch.b28c944c.png"
+  },
+  {
+    "revision": "a6aa6ef766e8d9c595b9fb41cbcfebaf",
+    "url": "/witty-mvp/index.html"
+  }
+];
